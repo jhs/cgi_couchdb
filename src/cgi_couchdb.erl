c01e0f5b17688a58179bfa6ea77215aad3a9fad4
Jason Smith <jhs@couchone.com>
2010-12-07 20:14:31 +0700

    Demonostrating streaming by sending the first attachment as a response

--- src/cgi_couchdb.erl
+++ src/cgi_couchdb.erl
@@ -9,9 +9,18 @@
 
 handle_cgi_req(Req, Db, DDoc)
     -> ?LOG_INFO("CGI REQUEST: ~p\n", [Req])
-    , ?LOG_INFO("Db ~p\n", [Db])
+    %, ?LOG_INFO("Db ~p\n", [Db])
     , ?LOG_INFO("DDoc ~p\n", [DDoc])
-    , couch_httpd:send_response(Req, 200, [{"Content-Type", "text/plain"}], <<"Hello, CGI!\r\n">>)
+    , Att = lists:nth(1, DDoc#doc.atts)
+    , {ok, Resp} = couch_httpd:start_chunked_response(Req, 200, [{"Content-Type", "text/plain"}])
+    , {ok, Resp} = couch_doc:att_foldl_decode(Att, fun folder/2, {ok, Resp})
+    , {ok, Resp} = couch_httpd:last_chunk(Resp)
+    .
+
+folder(Data, {ok, Resp}=State)
+    -> ?LOG_INFO("Streamer with ~p bytes", [iolist_size(Data)])
+    , couch_httpd:send_chunk(Resp, Data)
+    , State
     .
 
 % vim: sw=4 sts=4 et
