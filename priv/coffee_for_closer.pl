778d80b44171db32521216cc0ea66dfe46fd6484
Jason Smith <jhs@couchone.com>
2010-12-08 17:29:18 +0700

    Closer to CGI with a subprocess that will open the actual script but close stdout

---
+++ priv/coffee_for_closer.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+#
+# This is stupid. Erlang ports have no way to close the writer handle
+# and then read the reader handle. That would be useful to e.g. send
+# source code to a dynamic language and read what output comes back.
+# This tool opens the actual CGI program as a subprocess. Then it receives
+# from Erlang a "NNNN\n" preamble indicating the total data length. When
+# that much data has been forwarded to CGI, it closes the filehandle so
+# the subprocess can send results back to Erlang.
+
+use strict;
+
+open LOG, '>', '/tmp/log' or die $!;
+print LOG "Starting closer: $$: @ARGV\n";
+
+my $length = <STDIN>;
+chomp $length;
+print LOG "Reading $length bytes from STDIN\n";
+
+my ($buf, $got_size);
+my $CHUNK_SIZE = 4096;
+my $remaining = $length;
+
+while($remaining > 0) {
+  $got_size = read(STDIN, $buf, ($remaining < $CHUNK_SIZE) ? $remaining : $CHUNK_SIZE);
+  print LOG "Received $got_size from STDIN\n";
+  die "Received end-of-file, but $remaining bytes (out of total $length) expected" if($got_size == 0);
+  $remaining -= $got_size;
+  die "Received greater-than-expected data ($remaining, expected=$length)" if($remaining < 0);
+}
+
+print LOG "Remaining is just right: $remaining\n";
+
+print STDOUT "Content-Type: text/html\r\n\r\n";
+print STDOUT "<a href='/blah'>Link!</a><br>\n";
+print STDOUT "CGI: Thank you, I got it all\n";
+
+print LOG "Exiting\n";
+close LOG;
+
+exit(0);
--- src/cgi_couchdb.erl
+++ src/cgi_couchdb.erl
@@ -8,19 +8,61 @@
 -include("couch_db.hrl").
 
 handle_cgi_req(Req, Db, DDoc)
-    -> ?LOG_INFO("CGI REQUEST: ~p\n", [Req])
-    %, ?LOG_INFO("Db ~p\n", [Db])
-    , ?LOG_INFO("DDoc ~p\n", [DDoc])
-    , Att = lists:nth(1, DDoc#doc.atts)
+    -> ?LOG_INFO("CGI request", [])
+    , {ok, Subprocess} = cgi_subprocess(Req, Db, DDoc)
     , {ok, Resp} = couch_httpd:start_chunked_response(Req, 200, [{"Content-Type", "text/plain"}])
-    , {ok, Resp} = couch_doc:att_foldl_decode(Att, fun folder/2, {ok, Resp})
+    , {ok, Resp} = stream_from_subprocess(Resp, {ok, Subprocess})
     , {ok, Resp} = couch_httpd:last_chunk(Resp)
     .
 
-folder(Data, {ok, Resp}=State)
-    -> ?LOG_INFO("Streamer with ~p bytes", [iolist_size(Data)])
-    , couch_httpd:send_chunk(Resp, Data)
+cgi_subprocess(Req, Db, DDoc)
+    -> ?LOG_INFO("Subprocess for request: ~p\n", [Req])
+    , process_flag(trap_exit, true)
+    , ?LOG_INFO("Db ~p\n", [Db])
+    , ?LOG_INFO("DDoc ~p\n", [DDoc])
+    , Att = lists:nth(1, DDoc#doc.atts)
+    , ?LOG_INFO("Attachment length: ~p", [Att#att.att_len])
+    , Closer = filename:join(code:priv_dir(?MODULE), "coffee_for_closer.pl")
+    , Env = []
+    , Args = ["first", "second"]
+    , Port = open_port({spawn_executable, Closer}, [binary, stream, {args, Args}, {env, Env}]) % TODO: catch
+    , case Port
+        of Port when is_port(Port)
+            -> ?LOG_INFO("Sending code to subprocess: ~p", [self()])
+            , port_command(Port, [integer_to_list(Att#att.att_len), <<"\n">>])
+            , {ok, Port} = couch_doc:att_foldl_decode(Att, fun stream_to_subprocess/2, {ok, Port})
+        ; Error
+            -> exit({open_port_failed, Error, [{request, theRequest}]})
+        end
+    .
+
+stream_to_subprocess(Data, {ok, Subprocess}=State)
+    -> ?LOG_INFO("Sending ~p bytes to subprocess", [iolist_size(Data)])
+    , port_command(Subprocess, Data)
     , State
+    .
+
+stream_from_subprocess(Resp, {ok, Subprocess}=State)
+    -> ?LOG_INFO("Streaming response from subprocess: ~p", [self()])
+    , receive
+        {Subprocess, {data, Data}} when is_port(Subprocess)
+            -> ?LOG_INFO("Sending ~p bytes from subprocess", [iolist_size(Data)])
+            , couch_httpd:send_chunk(Resp, Data)
+            , stream_from_subprocess(Resp, State)
+        ; {'EXIT', Subprocess, Reason} when is_port(Subprocess)
+            % Done. Return the chunks in order.
+            -> ?LOG_INFO("Subprocess exited: ~p", [Reason])
+            , {ok, Resp}
+        ; {'EXIT', Pid, Reason} when is_pid(Pid)
+            -> ?LOG_ERROR("Linked process died", [])
+            , exit({linked_process_died, Pid, Reason})
+        ; Else
+            -> ?LOG_ERROR("Woa: ~p", [Else])
+            , exit({unknown_process_error, Else})
+        after 3000
+            -> ?LOG_ERROR("Subprocess timed out", [])
+            , exit(timeout)
+    end
     .
 
 % vim: sw=4 sts=4 et
