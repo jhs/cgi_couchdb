2e45fae7ba3f3a8c1ce0ee0a09c9a95256c6afb9
Jason Smith <jhs@couchone.com>
2010-12-08 19:17:55 +0700

    Actually figure out what to do based on the DDoc

--- src/cgi_couchdb.erl
+++ src/cgi_couchdb.erl
@@ -8,7 +8,52 @@
 -include("couch_db.hrl").
 
 handle_cgi_req(Req, Db, DDoc)
-    -> ?LOG_INFO("CGI request", [])
+    -> ?LOG_DEBUG("CGI request: ~p", [Req#httpd.path_parts])
+    , {DDocBody} = DDoc#doc.body
+    , case couch_util:get_value(<<"cgi">>, DDocBody)
+        of undefined
+            -> error({not_found, "Design document has no CGI definitions"})
+        ; {CgiDefinitions}
+            -> [_Db, <<"_design">>, _DDName, _CgiTrigger, CgiKey | PathInfoParts] = Req#httpd.path_parts
+            , ?LOG_DEBUG("CGI definition: ~p", [CgiDefinitions])
+            , case couch_util:get_value(CgiKey, CgiDefinitions)
+                of undefined
+                    -> error({not_found, io_lib:format("No such CGI definition: ~s", [CgiKey])})
+                ; {CgiDefinition}
+                    -> ProgramName = case couch_util:get_value(<<"exec">>, CgiDefinition)
+                        of undefined
+                            -> error({not_found, io_lib:format("No \"exec\" field in CGI definition: ~s", [CgiKey])})
+                        ; Unwelcome when not is_binary(Unwelcome)
+                            -> error({not_found, "CGI \"exec\" property must be a string"})
+                        ; Name -> Name
+                        end
+                    , Environment = case couch_util:get_value(<<"env">>, CgiDefinition)
+                        of undefined -> []
+                        ; null -> []
+                        ; {Env} -> [{binary_to_list(K), binary_to_list(V)} || {K, V} <- Env]
+                        ; _ -> error({not_found, "CGI \"env\" property must be undefined, null, or an object"})
+                        end
+
+                    , SourceCode = case couch_util:get_value(<<"code">>, CgiDefinition)
+                        of undefined -> error({not_found, "CGI \"code\" property must be defined"})
+                        ; Inlined when is_binary(Inlined)
+                            -> {inline, Inlined}
+                        ; {CodeObj}
+                            -> case couch_util:get_value(<<"attachment">>, CodeObj)
+                                of AttachmentName when is_list(AttachmentName)
+                                    -> {attachment, AttachmentName}
+                                ; _ -> error({not_found, "CGI \"code\" object is missing \"attachment\" definition"})
+                                end
+                        end
+
+                    % Ready! Run it!
+                    , run_cgi(Req, Db, DDoc, ProgramName, Environment, SourceCode)
+                end
+        end
+    .
+
+run_cgi(Req, Db, DDoc, ProgramName, Environment, SourceCode)
+    -> ?LOG_DEBUG("Running CGI ~p with ~p against ~p", [ProgramName, Environment, SourceCode])
     , {ok, Subprocess} = cgi_subprocess(Req, Db, DDoc)
     , {ok, Resp} = couch_httpd:start_chunked_response(Req, 200, [{"Content-Type", "text/plain"}])
     , {ok, Resp} = stream_from_subprocess(Resp, {ok, Subprocess})
@@ -16,10 +61,10 @@ handle_cgi_req(Req, Db, DDoc)
     .
 
 cgi_subprocess(Req, Db, DDoc)
-    -> ?LOG_INFO("Subprocess for request: ~p\n", [Req])
+    -> ?LOG_DEBUG("Subprocess for request: ~p\n", [Req])
     , process_flag(trap_exit, true)
-    , ?LOG_INFO("Db ~p\n", [Db])
-    , ?LOG_INFO("DDoc ~p\n", [DDoc])
+    %, ?LOG_INFO("Db ~p\n", [Db])
+    %, ?LOG_INFO("DDoc ~p\n", [DDoc])
     , Att = lists:nth(1, DDoc#doc.atts)
     , ?LOG_INFO("Attachment length: ~p", [Att#att.att_len])
     , Closer = filename:join(code:priv_dir(?MODULE), "coffee_for_closer.pl")
